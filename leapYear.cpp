1a8ec327ce829da9fa4dc291b67dfdb6c3c1cddb
aj2004 <kb.wiebe@shaw.ca>
2020-12-08 13:05:23 -0800

    Changed leapYear.cpp to return 0/1 instead of false/true

--- leapYear.cpp
+++ leapYear.cpp
@@ -12,8 +12,8 @@
 #include "leapYear.h"
 
 bool leapYear (unsigned int year){
-    if ((year % 4) != 0){ return false; }
-    else if ((year % 100) != 0){ return true; }
-    else if ((year % 400) == 0){ return true; }
-    else { return false; }
+    if ((year % 4) != 0){ return 0; }
+    else if ((year % 100) != 0){ return 1; }
+    else if ((year % 400) == 0){ return 1; }
+    else { return 0; }
 }
